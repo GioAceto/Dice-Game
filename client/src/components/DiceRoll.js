695b12b703f3a8ca58f813702b88657493b214d8
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-09-06 18:29:48 -0400

    create numberOptions array of die roll option objects

--- client/src/components/DiceRoll.js
+++ client/src/components/DiceRoll.js
@@ -1,6 +1,30 @@
 import React from 'react'
 
 const DiceRoll = () => {
+
+  const numberOptions = [
+    {
+      number: 1,
+      image: "test1"
+    },
+    {
+      number: 2,
+      image: "test2"
+    },
+    {
+      number: 3,
+      image: "test3"
+    },
+    {
+      number: 4,
+      image: "test4"
+    },
+    {
+      number: 5,
+      image: "test5"
+    }
+  ]
+
   return (
     <div className="container">
       <h1>You rolled a </h1>
