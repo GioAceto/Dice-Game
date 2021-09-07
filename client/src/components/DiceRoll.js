d75d813819bc60b85cbb3515060d8ea6c5ec2984
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-09-06 21:19:23 -0400

    change rollDie so it sets number and image states

--- client/src/components/DiceRoll.js
+++ client/src/components/DiceRoll.js
@@ -2,47 +2,55 @@ import React, { useState } from 'react';
 
 const DiceRoll = () => {
 
-  let [number, setNumber] = useState("")
+  let [number, setNumber] = useState("");
+  let [image, setImage] = useState("");
 
   const numberOptions = [
     {
       number: 1,
-      image: "test1"
+      image: "test1",
+      id: 1
     },
     {
       number: 2,
-      image: "test2"
+      image: "test2",
+      id: 2
     },
     {
       number: 3,
-      image: "test3"
+      image: "test3",
+      id: 3
     },
     {
       number: 4,
-      image: "test4"
+      image: "test4",
+      id: 4
     },
     {
       number: 5,
-      image: "test5"
+      image: "test5",
+      id: 5
     },
     {
       number: 6,
-      image: "test5"
+      image: "test5",
+      id: 6
     }
   ]
 
   const rollDie = () => {
     let randomNumber = Math.floor(Math.random() * numberOptions.length)
-    return numberOptions[randomNumber].number
+    setNumber(numberOptions[randomNumber].number)
+    setImage(numberOptions[randomNumber].image)
   }
 
   return (
     <div className="container">
       <h1>You rolled a {number}</h1>
       <div className="image-container">
-
+        <span>{image}</span>
       </div>
-      <button onClick={() => setNumber(rollDie)}>Roll</button>
+      <button onClick={rollDie}> Roll</button>
     </div>
   )
 }
